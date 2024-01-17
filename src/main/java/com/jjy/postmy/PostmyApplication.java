1061090510b3afca10d50df69d002e8008eeb804
JuYeon-Jin <juuju.00b@gmail.com>
2024-01-18 01:36:05 +0900

    first commit

---
+++ src/main/java/com/jjy/postmy/PostmyApplication.java
@@ -0,0 +1,13 @@
+package com.jjy.postmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmyApplication.class, args);
+	}
+
+}
