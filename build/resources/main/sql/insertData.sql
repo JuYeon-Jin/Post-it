cd15150954fc71c9f8e6a6467713e1e2c87b919e
JuYeon-Jin <juuju.00b@gmail.com>
2024-01-22 17:15:13 +0900

    main commit

---
+++ build/resources/main/sql/insertData.sql
@@ -0,0 +1,14 @@
+-- test01의 POST 데이터 삽입
+INSERT INTO POST (pinNo, title, content, dueDate)
+VALUES ('ee3a2428-b5bb-11ee-b788-21cc9dac88a2', '제목1', '내용1', '2024-01-22 18:00:00');
+INSERT INTO POST (pinNo, title, content, dueDate)
+VALUES ('ee3a2428-b5bb-11ee-b788-21cc9dac88a2', '제목2', '내용2', '2024-01-24 18:00:00');
+INSERT INTO POST (pinNo, title, content, dueDate)
+VALUES ('ee3a2428-b5bb-11ee-b788-21cc9dac88a2', '제목3', '내용3', '2024-01-27 18:00:00');
+INSERT INTO POST (pinNo, title, content, dueDate)
+VALUES ('ee3a2428-b5bb-11ee-b788-21cc9dac88a2', '제목4', '내용4', '2024-01-29 18:00:00');
+INSERT INTO POST (pinNo, title, content, dueDate)
+VALUES ('ee3a2428-b5bb-11ee-b788-21cc9dac88a2', '제목5', '내용5', '2024-02-03 18:00:00');
+-- 완료
+
+
