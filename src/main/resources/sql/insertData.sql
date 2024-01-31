29026ce6767bff2817eff0f2a9831820b4be23cb
JuYeon-Jin <juuju.00b@gmail.com>
2024-01-31 14:30:54 +0900

    ver1. last commit

--- src/main/resources/sql/insertData.sql
+++ src/main/resources/sql/insertData.sql
@@ -9,4 +9,7 @@ INSERT INTO POST (pinNo, title, content, dueDate)
 VALUES ('ee3a2428-b5bb-11ee-b788-21cc9dac88a2', '제목4', '내용4', '2024-01-29 18:00:00');
 INSERT INTO POST (pinNo, title, content, dueDate)
 VALUES ('ee3a2428-b5bb-11ee-b788-21cc9dac88a2', '제목5', '내용5', '2024-02-03 18:00:00');
--- 완료
+-- 완료 4af92291-b62b-11ee-b788-21cc9dac88a2
+INSERT INTO POST (pinNo, title, content, dueDate)
+VALUES ('4af92291-b62b-11ee-b788-21cc9dac88a2'
+, '제목1', '내용1', '2024-01-22 18:00:00');
